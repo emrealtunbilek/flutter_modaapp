20fca2ffb6dd587af0a3eeb9980aedf5a2bf43c3
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-19 23:29:42 +0300

    arayüz tasarımına başladık bölüm 2

--- lib/main.dart
+++ lib/main.dart
@@ -40,22 +40,103 @@ class _AnaSayfaState extends State<AnaSayfa> {
           ),
         ],
       ),
-
       body: ListView(
-        padding: EdgeInsets.only(top:10),
+        padding: EdgeInsets.only(top: 10),
         children: <Widget>[
+          //Üst taraftaki profil listesi
           Container(
-            color: Colors.blue.shade300,
-            height: 150,
+            //color: Colors.blue.shade300,
+            height: 140,
             width: double.infinity,
             child: ListView(
               padding: EdgeInsets.all(10),
               scrollDirection: Axis.horizontal,
               children: <Widget>[
                 listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                SizedBox(width: 30,),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                SizedBox(width: 30,),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.jpg'),
+                SizedBox(width: 30,),
+                listeElemani('assets/model1.jpeg', 'assets/chanellogo.jpg'),
+                SizedBox(width: 30,),
+                listeElemani('assets/model2.jpeg', 'assets/louisvuitton.jpg'),
+                SizedBox(width: 30,),
+                listeElemani('assets/model3.jpeg', 'assets/chloelogo.jpg'),
+                SizedBox(width: 30,),
               ],
             ),
+          ),
+
+          //Card
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Material(
+              borderRadius: BorderRadius.circular(16),
+              elevation: 4,
+              color: Colors.blue.shade300,
+              child: Container(
+                height: 450,
+                width: double.infinity,
+                padding: EdgeInsets.all(16),
+                child: Column(
+
+                  children: <Widget>[
+
+                    Row(
+                      children: <Widget>[
+
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              image: DecorationImage(
+                                  image: AssetImage('assets/model1.jpeg'),
+                                  fit: BoxFit.cover)
+                          ),
+                        ),
+                        SizedBox(width: 10,),
+
+                        Container(
+                          width: MediaQuery
+                              .of(context)
+                              .size
+                              .width - 160,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Daisy", style: TextStyle(
+                                  fontFamily: 'Montserrat',
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold),),
+                              SizedBox(height: 5,),
+                              Text('34 mins ago', style: TextStyle(
+                                  fontFamily: 'Montserrat', fontSize: 12,color: Colors.grey),)
+                            ],
+                          ),
+
+                        ),
+                        SizedBox(width: 10,),
+                        Icon(Icons.more_vert, color: Colors.grey, size: 22,)
+
+                      ],
+
+                    ),
+
+
+                  ],
+
+                ),
+
+
+              ),
+
+
+            ),
           )
+
+
         ],
       ),
     );
@@ -64,19 +145,49 @@ class _AnaSayfaState extends State<AnaSayfa> {
   Widget listeElemani(String imagePath, String logoPath) {
     return Column(
       children: <Widget>[
-
         Stack(
           children: <Widget>[
             Container(
               height: 75,
               width: 75,
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                image: DecorationImage(image: AssetImage(imagePath),fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(38),
+                image: DecorationImage(
+                    image: AssetImage(imagePath), fit: BoxFit.cover),
               ),
-            )
+            ),
+            Positioned(
+              top: 50,
+              left: 50,
+              child: Container(
+                width: 25,
+                height: 25,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12),
+                  image: DecorationImage(
+                      image: AssetImage(logoPath), fit: BoxFit.contain),
+                ),
+              ),
+            ),
           ],
         ),
+        SizedBox(height: 10,),
+
+        Container(
+          height: 30,
+          width: 75,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Colors.brown
+          ),
+          child: Center(
+            child: Text("Follow", style: TextStyle(
+                fontFamily: 'Montserrat',
+                fontSize: 14,
+                color: Colors.white
+            ),),
+          ),
+        )
 
 
       ],
