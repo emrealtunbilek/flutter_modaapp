b52f66cbc36f48676a2f18fbd42c504c1fb77794
Emre Altunbilek <emrealtunbilek@gmail.com>
2019-01-20 02:32:15 +0300

    detay sayfası da tamamlandı uygulama bitti

--- lib/detay.dart
+++ lib/detay.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 class Detay extends StatefulWidget {
-
   var imgPath;
 
   Detay({this.imgPath});
@@ -14,15 +13,143 @@ class _DetayState extends State<Detay> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Hero(
-        tag: widget.imgPath,
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage(widget.imgPath), fit: BoxFit.cover)
+      body: Stack(
+        children: <Widget>[
+          Hero(
+            tag: widget.imgPath,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(widget.imgPath), fit: BoxFit.cover)),
+            ),
           ),
-        ),
+          Positioned(
+            left: 15,
+            right: 15,
+            bottom: 15,
+            child: Material(
+              borderRadius: BorderRadius.circular(10),
+              elevation: 4,
+              child: Container(
+                height: 250,
+                width: MediaQuery.of(context).size.width - 30,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Container(
+                            height: 120,
+                            width: 100,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10),
+                                border: Border.all(
+                                  color: Colors.grey,
+                                ),
+                                image: DecorationImage(
+                                    image: AssetImage(
+                                      'assets/dress.jpg',
+                                    ),
+                                    fit: BoxFit.contain)),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              "LAMINATED",
+                              style: TextStyle(
+                                  fontSize: 22,
+                                  fontFamily: 'Montserrat',
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 5,
+                            ),
+                            Text(
+                              "Louis vuitton",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontFamily: 'Montserrat',
+                                  color: Colors.grey),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Container(
+                              height: 30,
+                              width: MediaQuery.of(context).size.width - 170,
+                              child: Text(
+                                "One button V-neck sling long-sleeved waist female stitching dress",
+                                style: TextStyle(
+                                    fontSize: 13,
+                                    fontFamily: 'Montserrat',
+                                    color: Colors.grey),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Divider(),
+                    Padding(
+                      padding:
+                          const EdgeInsets.only(left: 15, top: 10, bottom: 2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            '\$6500',
+                            style: TextStyle(
+                                fontFamily: 'Montserrat', fontSize: 22),
+                          ),
+                          Container(
+                            margin: EdgeInsets.only(right: 30),
+                            child: FloatingActionButton(
+                              onPressed: () {},
+                              child:
+                                  Center(child: Icon(Icons.arrow_forward_ios)),
+                              backgroundColor: Colors.brown,
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+
+          Positioned(
+            top: MediaQuery.of(context).size.height/2,
+            left: 50,
+            child: Container(
+              height: 40,
+              width: 130,
+              decoration: BoxDecoration(
+                color: Colors.black.withOpacity(0.4),
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Center(child: Text("LAMINATED",style: TextStyle(fontFamily: 'Montserrat', color: Colors.white,fontWeight: FontWeight.bold),)),
+                  Icon(Icons.arrow_forward_ios,color: Colors.white, size: 18,)
+                ],
+              ),
+            ),
+            
+          )
+
+        ],
       ),
     );
   }
